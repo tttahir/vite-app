167e27a6b1d3fd3692fdbd127c317b4849bc2b39
Tahir <tnur92@gmail.com>
2022-02-22 10:57:05 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react"
+import { render } from "react-dom"
+
+import "./index.css"
+
+import { App } from "./App"
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+)
