1caee3c3e19d9863c1f60041ad6b678a6e1c8af8
Tahir <tnur92@gmail.com>
2022-02-23 19:32:04 +0300

    test

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="react-scripts" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PREVIEW_URL: string
+  readonly VITE_API_URL: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
