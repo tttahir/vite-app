e03f97b0344266fb57cb7c7988e1d4b59a16195a
Tahir <tnur92@gmail.com>
2022-02-23 19:27:03 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>as
     </div>
   )
 }
