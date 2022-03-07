ea51ae141efbcf897d2cc63fdd742d6ba7b474d7
Tahir <tnur92@gmail.com>
2022-03-07 18:56:43 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { Select, SelectProps } from "antd"
+import RcSelect, { Option as RcOption } from "rc-select"
+
+const { Option } = Select
+
+const handleChange: SelectProps["onChange"] = (value) => {
+  console.log(`selected ${value}`)
+}
+
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="p-10">
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+        // prefixCls="qwe"
+        dropdownClassName="dropdown"
+        className="
+        box-border
+        m-0
+        p-0
+        text-black/80
+        text-sm
+        tabular-nums
+        list-none
+        relative
+        inline-block
+        cursor-pointer
+        "
+      >
+        {Array(5)
+          .fill(0)
+          .map((v, i) => (
+            <Option key={i} value={i}>
+              {i}
+            </Option>
+          ))}
+      </Select>
+      <RcSelect prefixCls="asd">
+        {Array(5)
+          .fill(0)
+          .map((v, i) => (
+            <RcOption key={i} value={i}>
+              {i}
+            </RcOption>
+          ))}
+      </RcSelect>
     </div>
   )
 }
